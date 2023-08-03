67aa9b35592f4cc5c21743b263cfdadeb6281aab
myykel <ebojimichael@gmail.com>
2023-08-03 19:03:23 +0100

    Used react Hook features useState usesEffect useMemo

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,11 +1,29 @@
 import logo from './logo.svg';
 import './main-page.css';
 import Header from './header';
+import { useEffect, useState, useMemo } from 'react';
 
 function App() {
+const [ allHouses,setAllHouses] = useState([])  
+  useEffect= (()=>{
+    const fetchHouses = async () =>{
+    const rsp=await fetch('./houses.json')
+    const houses = await rsp.json()
+      setAllHouses(houses)
+    };
+    fetchHouses()
+  },[])
+
+  const featuredHouse =  useMemo(()=>{
+    if(allHouses.lengh){
+      const randomIndex = Math.floor(Math.random() * allHouses.length);
+      return allHouses[randomIndex]
+    }
+  },[allHouses])
+ 
   return (
     <div className="container">
-      <Header/>
+      <Header subtitle = "Providing houses all over the world" title ="new title"/>
     </div>
   );
 }
