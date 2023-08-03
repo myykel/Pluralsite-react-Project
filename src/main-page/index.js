bf3c3fdccbb502dca4fc0fb2619e661c54730bfb
myykel <ebojimichael@gmail.com>
2023-08-03 18:13:38 +0100

    Added first component

---
+++ src/main-page/index.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './main-page.css';
+import Header from './header';
+
+function App() {
+  return (
+    <div className="container">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
