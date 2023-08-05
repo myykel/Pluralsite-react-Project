9fcd3e5719a637e044193af8521515cc076c1ef9
Michael Eboji <mac@macs-MacBook-Pro.local>
2023-08-05 08:27:03 +0100

    used react hook, custom hook, useNavigation and completed the project

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -4,31 +4,24 @@ import Header from './header';
 import { useEffect, useState, useMemo } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Use 'Routes' instead of 'Switch'
 import FeaturedHouse from './featured-house';
+import SearchResults from '../search-results';
+import HouseFilter from './house-filter';
+import HouseFromQuery from '../house/HouseFromQuery';
+import useHouses from '../hooks/useHouses';
+import useFeaturedHouse from '../hooks/useFeaturedHouse';
 
 function App() {
-  const [allHouses, setAllHouses] = useState([]);
-
-  useEffect(() => {
-    const fetchHouses = async () => {
-      const rsp = await fetch('./houses.json');
-      const houses = await rsp.json();
-      setAllHouses(houses);
-    };
-    fetchHouses();
-  }, []);
-
-  const featuredHouse = useMemo(() => {
-    if (allHouses.length) {
-      const randomIndex = Math.floor(Math.random() * allHouses.length);
-      return allHouses[randomIndex];
-    }
-  }, [allHouses]);
+  const allHouses = useHouses();
+  const featuredHouse = useFeaturedHouse(allHouses);
 
   return (
     <Router>
       <div className="container">
         <Header subtitle="Providing houses all over the world" title="new title" />
+        <HouseFilter allHouses={allHouses} />
         <Routes> {/* Wrap routes in 'Routes' component */}
+          <Route path="/searchresults/:country" element={<SearchResults allHouses={allHouses} />} />
+          <Route path="/house/:id" element={<HouseFromQuery allHouses={allHouses} />} />
           <Route path="/" element={<FeaturedHouse house={featuredHouse} />} /> {/* Use 'element' prop to define the component */}
         </Routes>
       </div>
